b453d7d9399f0875c2c694c38c17c0adafb7b888
Nicklas Utgaard <nutgaard@gmail.com>
2020-09-24 14:50:31 +0200

    [KAIZEN-0] gjøre det mulig med react-kode i oppdateringslogg-konfig

--- src/app/oppdateringslogg/config/config.tsx
+++ src/app/oppdateringslogg/config/config.tsx
@@ -1,3 +1,5 @@
+import * as React from 'react';
+import Lesmerpanel from 'nav-frontend-lesmerpanel';
 import { EnOppdateringslogg } from '../OppdateringsloggContainer';
 import OppdateringsloggKnappBilde from './img/oppdateringslogg-knapp.jpg';
 
@@ -8,10 +10,13 @@ export const OppdateringsloggProd: EnOppdateringslogg[] = [
         dato: new Date('2020-09-24 12:42'),
         aktiv: true,
         ingress: 'Nå kan du se nye ting som kommer i modia. Trykk på bjelleikonet for å se hva som er nytt.',
-        beskrivelse:
-            'Her vil du se nye oppdateringer sortert fra den nyeste til den eldste. ' +
-            'Hvis det er kommet en ny oppdatering vil det komme en rød varsellampe oppe  ' +
-            'i høyre hjørne av bjelleikonet.',
+        beskrivelse: (
+            <Lesmerpanel apneTekst={''} lukkTekst={''}>
+                Her vil du se nye oppdateringer sortert fra den nyeste til den eldste. Hvis det er kommet en ny
+                oppdatering vil det komme en rød varsellampe oppe i høyre hjørne av bjelleikonet{' '}
+                <img src={OppdateringsloggKnappBilde} alt="" />.
+            </Lesmerpanel>
+        ),
         src: OppdateringsloggKnappBilde
     }
 ];
